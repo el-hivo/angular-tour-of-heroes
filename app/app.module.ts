96132835bd2788d34e7ada00da02ea661f774529
Ivan Hernandez <ivan.hernandez@definityfirst.com>
2016-10-27 15:34:15 -0700

    Added http functionality

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,6 +1,11 @@
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule }   from '@angular/forms';
+import { HttpModule }    from '@angular/http';
+import { InMemoryWebApiModule } from 'angular-in-memory-web-api';
+import './rxjs-extensions';
+
+import { InMemoryDataService } from './in-memory-data.service';
 
 import { AppComponent }         from './app.component';
 import { HeroDetailComponent }  from './hero-detail.component';
@@ -8,18 +13,22 @@ import { HeroesComponent }      from './heroes.component';
 import { HeroService }          from './hero.service';
 import { DashboardComponent }   from './dashboard.component';
 import { AppRoutingModule }     from './app-routing.module';
+import { HeroSearchComponent }  from './hero-search.component';
 
 @NgModule({
   imports: [
     BrowserModule,
     FormsModule,
+    HttpModule,
+    InMemoryWebApiModule.forRoot(InMemoryDataService),
     AppRoutingModule
   ],
   declarations: [
     AppComponent,
     HeroDetailComponent,
     HeroesComponent,
-    DashboardComponent
+    DashboardComponent,
+    HeroSearchComponent
   ],
   bootstrap: [
     AppComponent
--- app/hero-detail.component.ts
+++ app/hero-detail.component.ts
@@ -18,7 +18,7 @@ export class HeroDetailComponent implements OnInit {
 
     hero: Hero;
 
-    ngOnInit() : void{
+    ngOnInit() : void {
         this.route.params.forEach((params: Params) => {
             let id = +params['id'];
             this.heroService.getHero(id)
@@ -26,7 +26,12 @@ export class HeroDetailComponent implements OnInit {
         });
     }
 
-    goBack(): void{
+    goBack(): void {
         this.location.back();
+    }
+
+    save(): void {
+        this.heroService.update(this.hero)
+            .then(() => this.goBack());
     }
 }
